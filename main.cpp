dbd71d8982a1f379eec90d9e1505f9a35531896c
AManOfFortune <samuel.muskovich@gmail.com>
2022-09-23 14:20:14 -0400

    Full functionality with fork()

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
-#include <unistd.h>
-#include <stdio.h>
-#include <filesystem>
-#include <string>
+#include <vector>
+#include <unistd.h> //Option parsing
+#include <filesystem> //File system traversal
+#include <wait.h> //Waiting for child processes
 
 // Takes a path and a fileName, and returns the path if that path points to the given file
 std::string compareLocationToFileName (std::string fileName, std::string filePath, bool caseInsensitive) {
@@ -56,45 +56,76 @@ std::string searchFileSystem(std::string locationPath, std::string fileName, boo
 }
 
 int main(int argc, char* args[]) {
-    int opt; // used to parse options
-    bool recursive = false; // true if option is set
-    bool caseInsensitive = false; // true if option is set
 
-    // used to determine at which index the searchLocation path begins
-    // with no options set, index = 1, otherwise it gets increased with each set option
-    int searchLocationIndex = 1; 
-    
-    // Parses options (option begins with "-", then a letter, allowed are "-R" and "-i")
-    while((opt = getopt(argc, args, "Ri")) != EOF) 
-    { 
-        searchLocationIndex++;
+    bool recursive = false; // true if "-R" is set
+    bool caseInsensitive = false; // true if "-i" is set
 
-        switch(opt) 
-        { 
-            case 'i': 
-                caseInsensitive = true;
+    std::string searchLocation = ""; //The first string that is not an option is the search location
+    std::vector<std::string> filesToSearch; //Every other string that is not an option after the first one is a filename to search
+
+    // Loops arguments to parse them
+    // Custom parsing implemented to enable user giving options at any position within the argument list
+    // Loop starts at index 1 since the initial program call at index[0] (f.ex. "./myprog") should be ignored
+    for(int i = 1; i < argc; i++) {
+        std::string argument = args[i];
+
+        //Valid options are "-i" and "-R"
+        if(argument == "-i") {caseInsensitive = true; continue;}
+        if(argument == "-R") {recursive = true; continue;}
+        //If another option is passed, write error message and return EXIT_FAILURE
+        if(argument.at(0) == '-') {std::cout << "Unknown option. Options are\n\"-R\" recursive search\n\"-i\" case insensitive search\n\n./myfind [-R] [-i] searchpath filename1 [filename2] ... [filenameN]"; return EXIT_FAILURE;}
+        //If searchLocation wasn't set yet, current argument is first non-option, meaning it must be the searchLocation
+        if(!searchLocation.length()) {searchLocation = argument; continue;}
+
+        //Every non-option string after searchLocation is a filename to search, so we add it to the vector
+        filesToSearch.push_back(argument);
+    }
+
+    // Loops all filenames to search
+    for(auto const& fileNameToSearch: filesToSearch) {
+
+        // Forks once for every file we are searching for
+        auto pid = fork();
+
+        std::string foundFilePath;
+        switch(pid) {
+            // Error, should not happen
+            case -1:
+                return EXIT_FAILURE;
+            
+            // Child process searches file
+            case 0:
+                // If file is found, foundFilePath is the path where that file exists, otherwise it is an empty string ""
+                foundFilePath = searchFileSystem(searchLocation, fileNameToSearch, recursive, caseInsensitive);
+
+                //Prints success message
+                if(foundFilePath.length())
+                    std::cout << getpid() << ": " << fileNameToSearch << ": " << foundFilePath << std::endl;
+                else
+                    std::cout << getpid() << ": " << fileNameToSearch << ": Not found" << std::endl;
+
+                // Returns (terminates) child process
+                return EXIT_SUCCESS;
+
+            // Parent process does nothing, it simply continues the loop
+            default:
                 break;
-            case 'R': 
-                recursive = true;
-                break; 
-        } 
-    } 
-
-    // After options are parsed, searchLocation is the next argument after that, meaning searchLocationIndex now points to the correct index
-    auto searchLocation = args[searchLocationIndex];
-
-    // Loops all arguments after searchLocation (which should all be fileNames)
-    for(int fileNameIndex = searchLocationIndex + 1; fileNameIndex < argc; fileNameIndex++) {
-
-        // If file is found, foundFilePath is the path where that file exists, otherwise it is an empty string ""
-        auto foundFilePath = searchFileSystem(searchLocation, args[fileNameIndex], recursive, caseInsensitive);
-
-        if(foundFilePath.length())
-            std::cout << args[fileNameIndex] << ": " << foundFilePath << std::endl;
-        else
-            std::cout << args[fileNameIndex] << ": Not found" << std::endl;
+        }
 
     }
 
-	return 0;
+    // Once all forks are done, the parent process waits for all its children to finish
+
+    /// !!!!!!!!!!!!!!!!!!!!!!!!! BUGGED !!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // Sometimes the parent process finishes before its children do, no clue why
+    // Actually, I'm not even sure if that is what's happening
+    // The terminal sometimes just enables user input again, and prints some child-process output slightly later
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    pid_t childpid;
+    while((childpid = waitpid(-1, NULL, WNOHANG))) {
+        if((childpid == -1) && (errno != EINTR))
+            break;
+    }
+
+	return EXIT_SUCCESS;
 }
